f62becbd45f976b89beb21789613409a1308ba15
krister vennes <krister.vennes@hotmail.com>
2018-05-03 22:54:44 +0200

    Add project files.

---
+++ BulletHell/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace BulletHell
+{
+    public class Bullet : Sprite
+    {
+        public float Rotation { get; set; }
+        public Rectangle Rectangle { get; private set; }
+
+        public Bullet(Vector2 vector2, Texture2D texture2D, SpriteBatch spriteBatch, float rotation) : base(vector2, texture2D, spriteBatch)
+        {
+            Rotation = rotation;
+        }
+
+        public override void Draw()
+        {
+            SpriteBatch.Draw(Texture2D, Vector2, null, null, new Vector2(Texture2D.Width / 2, Texture2D.Height / 2), (float)Rotation, null, Color.OrangeRed, SpriteEffects.None, 0);
+        }
+
+        public void RectangleUpdate()
+        {
+            Rectangle = new Rectangle((int)Vector2.X, (int)Vector2.Y, Texture2D.Width, Texture2D.Height);
+        }
+    }
+}
---
+++ BulletHell/Game1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BulletHell
+{
+    public class Main : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D EnemyCubeTexture;
+        Texture2D BulletTexture;
+
+        Enemy EnemyCube;
+        Enemy EnemyTwo;
+
+        public Main()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //Load textures.
+            EnemyCubeTexture = Content.Load<Texture2D>("Block");
+            BulletTexture = Content.Load<Texture2D>("WhiteBall");
+
+            EnemyCube = new Enemy(Vector2.One * 100, EnemyCubeTexture, BulletTexture, spriteBatch, 0.02f, 15);
+            EnemyTwo = new Enemy(Vector2.One * 200, EnemyCubeTexture, BulletTexture, spriteBatch, 0.02f, 15);
+        }
+
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            EnemyCube.Update(gameTime);
+            EnemyTwo.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin();
+
+            EnemyCube.Draw();
+            EnemyTwo.Draw();
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
